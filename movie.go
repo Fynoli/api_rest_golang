8db11b750973591420497e42326bc84cf63622bb
Marcelo Baldini <mbaldini@finnegans.com.ar>
2021-10-14 17:57:00 -0300

    Comentario respecto dl ea codificacion a JSON

--- movie.go
+++ movie.go
@@ -1,5 +1,8 @@
 package main
 
+/*Fijate que si los nombres de los campos no empiezan con mayusculas
+el encoder de JSON se chotea y te manda los campos vacios, y lo mismo el encoder
+Una cagada*/
 type Movie struct {
 	Name     string `json:"name"`
 	Year     int    `json:"year"`
