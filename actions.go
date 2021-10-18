58e3016958156d49afbc807e20c221ede42ec20d
Marcelo Baldini <mbaldini@finnegans.com.ar>
2021-10-18 15:06:07 -0300

    CRUD completo

--- actions.go
+++ actions.go
@@ -20,6 +20,18 @@ func getSession() *mgo.Session {
 	return session
 }
 
+func responseMovie(w http.ResponseWriter, status int, results Movie) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(results)
+}
+
+func responseMovies(w http.ResponseWriter, status int, results []Movie) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(results)
+}
+
 var collection = getSession().DB("curso_go").C("movies")
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -34,9 +46,7 @@ func MovieList(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	json.NewEncoder(w).Encode(results)
+	responseMovies(w, 200, results)
 }
 
 func MovieShow(w http.ResponseWriter, r *http.Request) {
@@ -59,7 +69,72 @@ func MovieShow(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	responseMovie(w, 200, result)
+
+}
+
+func MovieUpdate(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	movie_id := params["id"]
+
+	if !bson.IsObjectIdHex(movie_id) {
+		w.WriteHeader(404)
+		return
+	}
+
+	oid := bson.ObjectIdHex(movie_id)
+
+	decoder := json.NewDecoder(r.Body)
+
+	var movie_data Movie
+	err := decoder.Decode(&movie_data)
+
+	if err != nil {
+		panic(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	defer r.Body.Close()
+
+	document := bson.M{"_id": oid}            //Recupero el documento bson (binary json) correspondiente a la id pasada
+	change := bson.M{"$set": movie_data}      //Creo un documento bson con los datos de movie_data
+	err = collection.Update(document, change) //Piso el documento original con el nuevo en base sin cambiar el id
+
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	responseMovie(w, 200, movie_data)
+
+}
+
+type ResponseMessage struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func MovieDelete(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	movie_id := params["id"]
+
+	if !bson.IsObjectIdHex(movie_id) {
+		w.WriteHeader(404)
+		return
+	}
+
+	oid := bson.ObjectIdHex(movie_id)
+
+	err := collection.RemoveId(oid)
+
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	result := ResponseMessage{200, "La película con ID= " + movie_id + " ha sido borrada exitosamente"}
+	w.Header().Set("ContentType", "application/json")
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(result)
 
@@ -84,7 +159,5 @@ func MovieAdd(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	json.NewEncoder(w).Encode(movie_data) //Esto siempre va último
+	responseMovie(w, 200, movie_data)
 }
